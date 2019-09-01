7c015a8eb01df83631c01083c3592fa6768b1f5f
merlox <merunasgrincalaitis@gmail.com>
2019-09-01 13:33:42 +0200

    Added yargs for reading the port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@
  * The main server file receiving all incoming requests while sending the API ones
  * to the apiRouter.
  */
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
@@ -12,7 +11,17 @@ const apiRoutes = require('./routes/apiRoutes.js')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const mongoUrl = 'mongodb://merlox:merlox1@ds213178.mlab.com:13178/general'
-const port = 8000
+const yargs = require('yargs')
+const argv = yargs.option('port', {
+    alias: 'p',
+    description: 'Set the port to run this server on',
+    type: 'number',
+}).help().alias('help', 'h').argv
+if(!argv.port) {
+    console.log('Error, you need to pass the port you want to run this application on with npm start -- -p 8001')
+    process.exit(0)
+}
+const port = argv.port
 
 // Required to generate randomized JWT tokens
 process.env.SALT = 'express'
